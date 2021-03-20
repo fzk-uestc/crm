838ff1685e83d7b563dfa394edb4f26a599884ef
fzk <test@fzk.com>
2021-03-20 22:56:55 +0800

    the seventh commit

--- src/main/java/com/fzk/crm/exception/LoginException.java
+++ src/main/java/com/fzk/crm/exception/LoginException.java
@@ -2,7 +2,7 @@ package com.fzk.crm.exception;
 
 /**
  * @author fzkstart
- * @create 2021-02-23 19:46
+ * @date 2021-02-23 19:46
  */
 public class LoginException extends Exception{
     public LoginException(String msg){
